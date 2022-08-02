c15f8468d2a6b1936bee32f4fdd4e14a4a4117c8
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-08-02 15:25:37 +0200

    Fixing responsive by adding new class => bot__row

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -4,23 +4,33 @@ const calculator = `
             <span class="calc__result"></span>
         </div>
         <div class="calculator__bot">
-            <span data-key="103" class="calc__numb">7</span>
-            <span data-key="104" class="calc__numb">8</span>
-            <span data-key="105" class="calc__numb">9</span>
-            <span data-key="111" class="calc__numb calc__numb--op">/</span>
-            <span data-key="100" class="calc__numb">4</span>
-            <span data-key="101" class="calc__numb">5</span>
-            <span data-key="102" class="calc__numb">6</span>
-            <span data-key="106" class="calc__numb calc__numb--op">*</span>
-            <span data-key="97" class="calc__numb">1</span>
-            <span data-key="98" class="calc__numb">2</span>
-            <span data-key="99" class="calc__numb">3</span>
-            <span data-key="109" class="calc__numb calc__numb--op">-</span>
-            <span data-key="96" class="calc__numb">0</span>
-            <span data-key="110" class="calc__numb">.</span>
-            <span data-key="8" class="calc__numb calc__numb--ac">AC</span>
-            <span data-key="107" class="calc__numb calc__numb--op">+</span>
-            <span data-key="13" class="calc__numb calc__numb--equ">=</span>
+          <div class="bot__row">
+              <span data-key="103" class="calc__numb">7</span>
+              <span data-key="104" class="calc__numb">8</span>
+              <span data-key="105" class="calc__numb">9</span>
+              <span data-key="111" class="calc__numb calc__numb--op">/</span>
+          </div>
+          <div class="bot__row">
+              <span data-key="100" class="calc__numb">4</span>
+              <span data-key="101" class="calc__numb">5</span>
+              <span data-key="102" class="calc__numb">6</span>
+              <span data-key="106" class="calc__numb calc__numb--op">*</span>
+          </div>
+          <div class="bot__row">
+              <span data-key="97" class="calc__numb">1</span>
+              <span data-key="98" class="calc__numb">2</span>
+              <span data-key="99" class="calc__numb">3</span>
+              <span data-key="109" class="calc__numb calc__numb--op">-</span>
+          </div>
+          <div class="bot__row">
+              <span data-key="96" class="calc__numb">0</span>
+              <span data-key="110" class="calc__numb">.</span>
+              <span data-key="8" class="calc__numb calc__numb--ac">AC</span>
+              <span data-key="107" class="calc__numb calc__numb--op">+</span>
+          </div>
+          <div class="bot__row">
+              <span data-key="13" class="calc__numb calc__numb--equ">=</span>
+          </div>
         </div>
     </main>`;
 document.querySelector("body").insertAdjacentHTML("afterbegin", calculator);
